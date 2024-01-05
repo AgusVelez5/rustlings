e5ff9062d23e23038be589ea4cb7319e6e5e32de
agus <agustinmaurasvelez@gmail.com>
2024-01-05 11:02:20 -0300

    Add 06

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(value: u32) -> u32 { 
+    if value > 40 {
+        return value
+    } 
+    value * 2
+}
 
 // Don't modify this function!
 #[test]
